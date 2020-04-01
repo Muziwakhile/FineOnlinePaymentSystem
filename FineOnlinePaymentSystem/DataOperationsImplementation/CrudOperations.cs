71a6dfeb65574a4dadfa3367557740c4066dddda
Mzwakhile <Mzwakhilet@gmail.com>
2020-04-01 11:46:13 +0200

    Added implementation for CRUD operations interfaces

---
+++ FineOnlinePaymentSystem/DataOperationsImplementation/CrudOperations.cs
@@ -0,0 +1,45 @@
+﻿using FineOnlinePaymentSystem.Data;
+using FineOnlinePaymentSystem.DataOpsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FineOnlinePaymentSystem.DataOperationsImplementation
+{
+    public class CrudOperations<T> : IdataOps<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+        public CrudOperations(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+        public virtual void Delete(T model)
+        {
+            dbContext.Remove<T>(model);
+            dbContext.SaveChanges();
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return dbContext.Set<T>().ToList<T>();
+        }
+
+        public virtual T GetById(int Id)
+        {
+            return dbContext.Find<T>(Id);
+        }
+
+        public virtual void Insert(T model)
+        {
+            dbContext.Add<T>(model);
+            dbContext.SaveChanges();
+        }
+
+        public virtual void Update(T model)
+        {
+            dbContext.Update<T>(model);
+            dbContext.SaveChanges();
+        }
+    }
+}
