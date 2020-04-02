81d3702e2d830e7383b4007a42dd0195724a58ba
Mzwakhile <Mzwakhilet@gmail.com>
2020-04-02 16:00:28 +0200

    Added Offender controller and created the index view

---
+++ FineOnlinePaymentSystem/Controllers/OffendersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FineOnlinePaymentSystem.Data;
+using FineOnlinePaymentSystem.DataOperationsImplementation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FineOnlinePaymentSystem.Controllers
+{
+    [Authorize(Roles ="SuperAdmin")]
+    public class OffendersController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly OffenderOps offenderOps;
+
+        public OffendersController(ApplicationDbContext _context)
+        {
+            context = _context;
+            offenderOps = new OffenderOps(context);
+        }
+
+        public IActionResult Index()
+        {
+            return View(offenderOps.GetAll());
+        }
+    }
+}
