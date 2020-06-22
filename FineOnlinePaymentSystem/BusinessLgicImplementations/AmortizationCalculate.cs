d1366769249dac834c91eb57736797b78df37fb6
Mzwakhile <Mzwakhilet@gmail.com>
2020-06-22 16:34:02 +0200

    Created Interface for claculating amortization and also a class to implement this interface

---
+++ FineOnlinePaymentSystem/BusinessLgicImplementations/AmortizationCalculate.cs
@@ -0,0 +1,44 @@
+﻿using FineOnlinePaymentSystem.BusinessLogicInterfaces;
+using FineOnlinePaymentSystem.DataOpsInterfaces;
+using FineOnlinePaymentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FineOnlinePaymentSystem.BusinessLgicImplementations
+{
+    public class AmortizationCalculate : IAmortizationCalculate
+    {
+        private readonly IdataOps<AmortizationSettings> amortizationSettings;
+        private int daysOverstayed;
+        private readonly int daysbeforeAmortization;
+        private readonly int PercentPerday;
+
+        public AmortizationCalculate(IdataOps<AmortizationSettings> _amortizationSettings)
+        {
+            amortizationSettings = _amortizationSettings;
+            daysbeforeAmortization = amortizationSettings.GetById(1).DaysBeforeAmortization;
+            PercentPerday = amortizationSettings.GetById(1).PercentPerDay;
+        }
+
+        public decimal AmortizationAmount(Case _case, Fine fine)
+        {
+            int totaldays = ((TimeSpan)(_case.CourtDate - _case.DateOfArrest)).Days;
+            daysOverstayed = totaldays - daysbeforeAmortization;
+            int percent = PercentPerday * daysOverstayed;
+
+            return (percent/100 * fine.Amount);
+        }
+
+       
+        public int DaysOverstayed(Case _case)
+        {
+            int totaldays = ((TimeSpan)(_case.CourtDate - _case.DateOfArrest)).Days;
+
+            daysOverstayed = totaldays - daysbeforeAmortization;
+
+            return daysOverstayed;
+        }
+    }
+}
---
+++ FineOnlinePaymentSystem/BusinessLogicInterfaces/IAmortizationCalculate.cs
@@ -0,0 +1,14 @@
+﻿using FineOnlinePaymentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FineOnlinePaymentSystem.BusinessLogicInterfaces
+{
+    interface IAmortizationCalculate
+    {
+        decimal AmortizationAmount(Case _case,Fine fine);
+        int DaysOverstayed(Case _case);
+    }
+}
