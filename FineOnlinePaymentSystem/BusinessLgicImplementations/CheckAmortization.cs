cc3916923ed0aca2592875e3654a03385fd1e5fa
Mzwakhile <Mzwakhilet@gmail.com>
2020-06-22 16:33:16 +0200

    Created Interfaces for checking if amortization is applicable and also created an implementation class for this

---
+++ FineOnlinePaymentSystem/BusinessLgicImplementations/CheckAmortization.cs
@@ -0,0 +1,45 @@
+﻿using FineOnlinePaymentSystem.BusinessLogicInterfaces;
+using FineOnlinePaymentSystem.DataOperationsImplementation;
+using FineOnlinePaymentSystem.DataOpsInterfaces;
+using FineOnlinePaymentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FineOnlinePaymentSystem.BusinessLgicImplementations
+{
+    public class CheckAmortization : ICheckAmortization
+    {
+        private readonly IdataOps<AmortizationSettings> crudOps;
+
+        public CheckAmortization(IdataOps<AmortizationSettings> crudOperations)
+        {
+            crudOps = crudOperations;
+        }
+        public bool CheckCaseDates(Case model)
+        {
+            if (model.DateOfArrest != null && model.CourtDate != null )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool CheckAmortizationStatus(Case model)
+        {
+                int datedif = ((TimeSpan)(model.CourtDate - model.DateOfArrest)).Days;
+                if (datedif > crudOps.GetById(1).DaysBeforeAmortization)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+        }
+    }
+}
---
+++ FineOnlinePaymentSystem/BusinessLogicInterfaces/ICheckAmortization.cs
@@ -0,0 +1,15 @@
+﻿using FineOnlinePaymentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FineOnlinePaymentSystem.BusinessLogicInterfaces
+{
+    public interface ICheckAmortization
+    {
+        bool CheckCaseDates(Case model);
+        bool CheckAmortizationStatus(Case model);
+
+    }
+}
