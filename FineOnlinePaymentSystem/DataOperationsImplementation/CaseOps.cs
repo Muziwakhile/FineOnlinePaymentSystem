6ffb065b8ea4f6b21475fb84b81c6808added46b
Mzwakhile <Mzwakhilet@gmail.com>
2020-07-01 14:58:45 +0200

    Added fuction to retrieve a list of case numbers

--- FineOnlinePaymentSystem/DataOperationsImplementation/CaseOps.cs
+++ FineOnlinePaymentSystem/DataOperationsImplementation/CaseOps.cs
@@ -1,6 +1,7 @@
 ﻿using FineOnlinePaymentSystem.Data;
 using FineOnlinePaymentSystem.DataOpsInterfaces;
 using FineOnlinePaymentSystem.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,20 @@ namespace FineOnlinePaymentSystem.DataOperationsImplementation
         public Case SearchByCaseNumber(int caseNumber)
         {
             return context.Cases.Where<Case>(c => c.CaseNumber == caseNumber).FirstOrDefault();
+        }
+
+
+        public List<int> ListByCaseNUmber2(int caseNumber)
+        {
+            var nums = new List<int>();
+            var result = context.Cases.FromSqlRaw($"Select * from Cases where CaseNumber like '%{caseNumber}%'").ToList();
+
+            foreach (var item in result)
+            {
+                nums.Add(item.CaseNumber);
+            }
+
+            return nums;
         }
 
         public List<Case> SearchByStatus(int status)
