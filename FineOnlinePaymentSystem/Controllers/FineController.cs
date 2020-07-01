97528ab4d4c46c555caf9bd963bbb40bd0dd6b68
Mzwakhile <Mzwakhilet@gmail.com>
2020-07-01 15:02:40 +0200

    Added Fine Controller,
    added action for creating a fine
    
    added action for handling case number autocomplete ajax call
    
    added action for handling PIN autocomplete ajax calls

---
+++ FineOnlinePaymentSystem/Controllers/FineController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FineOnlinePaymentSystem.Data;
+using FineOnlinePaymentSystem.DataOperationsImplementation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FineOnlinePaymentSystem.Controllers
+{
+    public class FineController : Controller
+    {
+        private readonly CaseOps caseOps;
+        private readonly CaseOffenderOps caseOffenders;
+
+        public FineController(ApplicationDbContext _context)
+        {
+            caseOps = new CaseOps(_context);
+            caseOffenders = new CaseOffenderOps(_context);
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult CreateFine()
+        {
+            return View();
+        }
+
+
+
+
+
+        [HttpGet]
+        public JsonResult ListByCaseNUmber2(int caseNumber)
+        {
+
+            var result = caseOps.ListByCaseNUmber2(caseNumber);
+
+            if (result != null)
+            {
+                return Json(result);
+            }
+            else
+            {
+                return Json(result);
+            }
+
+        }
+
+
+        [HttpGet]
+        public JsonResult ListByPIN(string PIN,int caseNumber)
+        {
+
+            var result = caseOffenders.ListByOffenderPIN(PIN,caseNumber);
+
+            if (result != null)
+            {
+                return Json(result);
+            }
+            else
+            {
+                return Json(result);
+            }
+
+        }
+    }
+}
