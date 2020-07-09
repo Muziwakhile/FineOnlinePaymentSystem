aceea3f736a49adc7b4c8b5f89b7a12d6a2f366c
Mzwakhile <Mzwakhilet@gmail.com>
2020-07-09 13:24:51 +0200

    Added Fine Operations to override getAll() so that it returns only fines that are pending not fines that are paid

---
+++ FineOnlinePaymentSystem/DataOperationsImplementation/FineOps.cs
@@ -0,0 +1,25 @@
+﻿using FineOnlinePaymentSystem.Data;
+using FineOnlinePaymentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FineOnlinePaymentSystem.DataOperationsImplementation
+{
+    public class FineOps:CrudOperations<Fine>
+    {
+        private readonly ApplicationDbContext context;
+
+        public FineOps(ApplicationDbContext _context):base(_context)
+        {
+            context = _context;
+        }
+        
+
+        public override List<Fine> GetAll()
+        {
+            return base.GetAll().Where<Fine>(c => c.FineStatus.FineStatusID == 1).ToList<Fine>();
+        }
+    }
+}
