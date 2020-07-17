63b6b4e44748b1d898f78352d1bb3110220c0f95
Mzwakhile <Mzwakhilet@gmail.com>
2020-07-17 02:41:00 +0200

    Added Migration to seed FinePaymentStatuses

--- FineOnlinePaymentSystem/FineOnlineSystem/Data/Migrations/20200717004013_SeedingFinePaymentStatuses.Designer.cs
+++ FineOnlinePaymentSystem/FineOnlineSystem/Data/Migrations/20200717004013_SeedingFinePaymentStatuses.Designer.cs
@@ -4,14 +4,16 @@ using FineOnlinePaymentSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace FineOnlinePaymentSystem.Data.Migrations
+namespace FineOnlinePaymentSystem.FineOnlineSystem.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200717004013_SeedingFinePaymentStatuses")]
+    partial class SeedingFinePaymentStatuses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FineOnlinePaymentSystem/FineOnlineSystem/Data/Migrations/20200717004013_SeedingFinePaymentStatuses.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FineOnlinePaymentSystem.FineOnlineSystem.Data.Migrations
+{
+    public partial class SeedingFinePaymentStatuses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "FinePaymentStatuses",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 1, "Pending" });
+
+            migrationBuilder.InsertData(
+                table: "FinePaymentStatuses",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 2, "Paid" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "FinePaymentStatuses",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "FinePaymentStatuses",
+                keyColumn: "ID",
+                keyValue: 2);
+        }
+    }
+}
